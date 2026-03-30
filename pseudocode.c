9429d00d0f91cf9702fc406b759501b33cee3c51
Vladimir Abubakirov <abubakirov04@mail.ru>
2026-03-30 23:45:59 +0300

    Add presentation and func pseudocode
    Signed-off-by: Vladimir Abubakirov <abubakirov04@mail.ru>

---
+++ pseudocode.c
@@ -0,0 +1,77 @@
+typedef union {
+    struct {
+        uint32_t ReservedPos;
+        uint32_t WriterCnt;
+    } S;
+    uint64_t U;
+} SHADOW_W_POS;
+
+typedef struct {
+    uint32_t ReadPos;
+    uint32_t WritePos;
+    uint64_t IsOverflow;
+    SHADOW_W_POS ShWPos;
+    char Buffer[0]; 
+} LOCKLESS_RING_BUFFER;
+
+uint32_t LrbReserve (
+    LOCKLESS_RING_BUFFER *Rb,
+    const uint32_t MsgSize,
+    SHADOW_W_POS *CurrShadowPos
+) {
+    for (;;) {
+        Curr->U = Rb->ShWPos.U;
+        SHADOW_W_POS NextShadowPos = {
+            .S.ReservedPos = Curr->S.ReservedPos + MsgSize,
+            .S.WritePos = Curr->S.WriterCnt + 1
+        };
+
+        if (atomic_compare_exchange(&Rb->ShWPos, &Curr->U, Next.U)) {
+            // SHADOW_W_POS была обновлена в буфере
+            // Таким образом, гарантируется, что ни один другой поток
+            // не будет копировать своё сообщение в занятую память.
+            return Curr->S.ReservedPos;
+        }
+    }
+}
+
+uint32_t LrbReserve (
+    LOCKLESS_RING_BUFFER *Rb,
+    const uint32_t MsgSize,
+    SHADOW_W_POS *Curr
+) {
+    for (;;) {
+        Curr->U = Rb->ShWPos.U;
+        SHADOW_W_POS Next = {
+            .S.ReservedPos = Curr->S.ReservedPos + MsgSize,
+            .S.WritePos = Curr->S.WriterCnt + 1
+        };
+
+        if (atomic_compare_exchange(&Rb->ShWPos, &Curr->U, Next.U)) {
+            // SHADOW_W_POS была обновлена в буфере
+            // Таким образом, гарантируется, что ни один другой поток
+            // не будет копировать своё сообщение в занятую память.
+            return Curr->S.ReservedPos;
+        }
+    }
+}
+
+uint32_t LrbCommit (
+    LOCKLESS_RING_BUFFER *Rb,
+    SHADOW_W_POS *Curr
+) {
+    for (;;) {
+        SHADOW_W_POS NextShadowPos = {
+            .S.ReservedPos = Curr->S.ReservedPos,
+            .S.WritePos = Curr->S.WriterCnt - 1
+        };
+
+        if (NextShadowPos.S.WriterCnt == 0) {
+            // все сообщения до текущего закончили копирование
+            Rb->WritePos = Curr->S.ReservedPos;
+        }
+        if (atomic_compare_exchange(&Rb->ShWPos, &Curr->U, Next.U)) {
+            return;
+        }
+    }
+}
